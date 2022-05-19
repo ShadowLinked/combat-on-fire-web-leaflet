c1f3824a00df769709d1ff216da21e8f2da4c047
ShadowLinked <64839579+ShadowLinked@users.noreply.github.com>
2022-05-19 22:35:50 +0200

    published update

---
+++ data/InformalShelters_4.js
@@ -0,0 +1,28 @@
+var json_InformalShelters_4 = {
+"type": "FeatureCollection",
+"name": "InformalShelters_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Shelter": "Beth Rogelim", "Address": "22 Alfred St, Cape Town City Centre, Cape Town", "x": 18.4221, "y": -33.9145 }, "geometry": { "type": "Point", "coordinates": [ 18.422100000414734, -33.914500000073971 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Culemborg Safe Space", "Address": "Under Culemborg bridge", "x": 18.433403, "y": -33.925001 }, "geometry": { "type": "Point", "coordinates": [ 18.433403000048489, -33.925000999595476 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Elim Night Shelter", "Address": "47 3rd Ave, Elsies River Industrial, Cape Town", "x": 18.572, "y": -33.9124 }, "geometry": { "type": "Point", "coordinates": [ 18.571870715704591, -33.912164836052874 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Happy Valley", "Address": "689 Palace Hill Rd, Simon's Town, Cape Town", "x": 18.4248, "y": -34.1891 }, "geometry": { "type": "Point", "coordinates": [ 18.424799999920101, -34.189099999963993 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Bellville)", "Address": "2 Chatl Malaw Street, Bellville, Cape Town", "x": 18.6261, "y": -33.9053 }, "geometry": { "type": "Point", "coordinates": [ 18.626100000114953, -33.905300000293828 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Claremont)", "Address": "5 Fir St, Claremont, Cape Town, Cape Town", "x": 18.4667, "y": -33.9773 }, "geometry": { "type": "Point", "coordinates": [ 18.466700000404746, -33.977300000293802 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (District Six)", "Address": "20 Selkirk St, Woodstock, Cape Town", "x": 18.435006, "y": -33.92813 }, "geometry": { "type": "Point", "coordinates": [ 18.434921904229089, -33.928406904087502 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Kalk Bay)", "Address": "139 Main Road, Kalk Bay, Cape Town", "x": 18.4478, "y": -34.129 }, "geometry": { "type": "Point", "coordinates": [ 18.447800000269876, -34.129000000148871 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Kensington)", "Address": "Corner of 13th Avenue and Dapper Road, Kensington\/Factreton, 7405", "x": 18.515295, "y": -33.913831 }, "geometry": { "type": "Point", "coordinates": [ 18.515271039396648, -33.915536399937508 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Kraaifontein)", "Address": "20 Van Der Ross Rd, Street, Cape Town", "x": 18.7119, "y": -33.8593 }, "geometry": { "type": "Point", "coordinates": [ 18.711899999785214, -33.85929999959432 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Moira HH)", "Address": "107 Chapel Street, Woodstock, Cape Town", "x": 18.437318, "y": -33.928581 }, "geometry": { "type": "Point", "coordinates": [ 18.437316318356707, -33.928836695540681 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Napier St.)", "Address": "2 Napier St, Green Point, Cape Town", "x": 18.4203, "y": -33.9141 }, "geometry": { "type": "Point", "coordinates": [ 18.420300000144977, -33.914099999614308 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Retreat)", "Address": "99 10th Ave, Cafda Village, Cape Town", "x": 18.4784, "y": -34.0582 }, "geometry": { "type": "Point", "coordinates": [ 18.478400000359787, -34.05819999972919 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Haven (Wynberg)", "Address": "16 Piers Rd, Wynberg, Cape Town", "x": 18.4678, "y": -34.0069 }, "geometry": { "type": "Point", "coordinates": [ 18.467799999870124, -34.006900000133903 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Loaves & Fishes", "Address": "1 Chatham Rd, Observatory, Cape Town", "x": 18.466392, "y": -33.929868 }, "geometry": { "type": "Point", "coordinates": [ 18.467321071081184, -33.930476694707622 ] } },
+{ "type": "Feature", "properties": { "Shelter": "MES", "Address": "3 Davies Avenue, Bellville, Cape Town", "x": 18.6252, "y": -33.9031 }, "geometry": { "type": "Point", "coordinates": [ 18.624553564919204, -33.899167015256758 ] } },
+{ "type": "Feature", "properties": { "Shelter": "OASIS", "Address": "28 Schaapkraal Road, Cape Town", "x": 18.534193, "y": -34.035447 }, "geometry": { "type": "Point", "coordinates": [ 18.528740384043914, -34.039137689352785 ] } },
+{ "type": "Feature", "properties": { "Shelter": "OWL Haven Shelter", "Address": "9 Polaris Rd, Lansdowne, Cape Town", "x": 18.5118, "y": -33.9915 }, "geometry": { "type": "Point", "coordinates": [ 18.511800000070036, -33.991500000423699 ] } },
+{ "type": "Feature", "properties": { "Shelter": "SW Night Shelter", "Address": "71 Church St, Somerset West, Cape Town", "x": 18.8425, "y": -34.0895 }, "geometry": { "type": "Point", "coordinates": [ 18.842499999675297, -34.089499999724183 ] } },
+{ "type": "Feature", "properties": { "Shelter": "The Ark", "Address": null, "x": 18.680501, "y": -33.995437 }, "geometry": { "type": "Point", "coordinates": [ 18.68041073174717, -34.013421856485841 ] } },
+{ "type": "Feature", "properties": { "Shelter": "Youth Solutions", "Address": "Cnr Russel & Chapel street, Zonnebloem, Cape town,", "x": 18.4352, "y": -33.9293 }, "geometry": { "type": "Point", "coordinates": [ 18.435200000179936, -33.929299999994001 ] } }
+]
+}
